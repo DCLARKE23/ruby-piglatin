6639d54af0e900653f71fe8ef397bab239b8786d
DCLARKE23 <dylanjclarke18@gmail.com>
2024-03-13 11:17:18 -0400

    Added translate_word and translate_string methods.

--- pig.rb
+++ pig.rb
@@ -1,5 +1,34 @@
 # English -> Pig Latin
 
-# Variables
+puts "Enter a word or string you would like to be translated."
+response = gets
 
-vowel_suffix = ['']
+def translate_word(word)
+    suffix = 'ay'
+    vowel_suffix = ['hay', 'way', 'nay', 'yay']
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    first_letter = word[0].downcase()
+    if vowels.include?(first_letter)
+        puts word.concat(vowel_suffix[rand(0...3)]).delete("\n")
+    else
+        consonants = []
+        for i in 0...word.length()
+            if vowels.include?(word[i]) == false
+                consonants << word[i]
+            else
+                break
+            end
+        end
+        "#{word[consonants.length..-1].delete("\n") + consonants.join + suffix}"
+    end
+end
+
+def translate_string(string)
+    words = string.split(" ")
+    new_string = words.map {|word| translate_word(word)}
+    new_string.join(" ")
+end
+
+if __FILE__ == $0
+    puts translate_string(response)
+end
